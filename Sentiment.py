f03ca01ab4122663d1100c0d8ef2fc38c2e9896d
harsha <khurdhulaharshavardhan@gmail.com>
2023-04-29 14:44:56 -0400

    adds fucntionality to transform user input.

--- Sentiment.py
+++ Sentiment.py
@@ -11,7 +11,11 @@ class Sentiment():
     #Sentiment Attributes:
     __vectorizer = None
     __model = None
-
+    __postivie_proba = None
+    __negative_proba = None
+    __postivie_log_proba = None
+    __negative_log_proba = None
+    __prediction = None
     def __init__(self) -> None:
         """
         Constructor is responsible for loading the following:
@@ -21,8 +25,24 @@ class Sentiment():
         try:
             self.__vectorizer = load("fitted_vectorizer.joblib")
             self.__model = load("logisitic_regression.joblib")
+
+            #Initialize the attributes to their types.
+            self.__postivie_proba = float()
+            self.__negative_proba = float()
+            self.__postivie_log_proba = float()
+            self.__negative_log_proba = float()
         except Exception as e:
             print("[ERR] The following error occured while trying to initialize Sentiment(): "+str(e))
 
+    def get_sentiment(self, user_input: str) -> dict:
+        """
+        Get sentiment is the core method of the Sentiment module, that receives the user input text, and yields the output.
+        """
+        try:
+            user_input_transformed = self.__vectorizer.transform([user_input]) #We represent the user input in sparse matrix.
+            print(user_input_transformed)
+        except Exception as e:
+            print("[ERR] The following error occured while trying to determine sentiment of your input: "+str(e))
 
-Sentiment()
+obj = Sentiment()
+obj.get_sentiment("Hello what is up my g?")
